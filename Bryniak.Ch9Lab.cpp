1c8b0aea40fcccff5c6ec6a8c73eceb7ebab5d7f
marsnebulasoup <marsnebulasoup@gmail.com>
2022-07-05 15:22:00 -0500

    Implemented song structure

--- Bryniak.Ch9Lab.cpp
+++ Bryniak.Ch9Lab.cpp
@@ -14,6 +14,18 @@ using namespace std;
 const string TITLE = "Playlist Sorter";
 const string DESCRIPTION = "This program prompts the user for a list of song/artist names and sorts and displays them.";
 
+struct Song
+{
+  string title;
+  string artist;
+
+  Song(string title = EMPTY_VAL, string artist = EMPTY_VAL)
+  {
+    Song::title = title != "" ? title : EMPTY_VAL;
+    Song::artist = artist != "" ? artist : EMPTY_VAL;
+  }
+};
+
 
 int main() {
   return 0;
